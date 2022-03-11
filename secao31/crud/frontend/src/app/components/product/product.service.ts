75a307fafa3eac95c93ca037be14bdc10f5534de
Olga Maria dos Santos Gonçalves <olgamariasssantos@gmail.com>
2022-03-10 21:59:08 -0300

    finalização da aba de angular

--- secao31/crud/frontend/src/app/components/product/product.service.ts
+++ secao31/crud/frontend/src/app/components/product/product.service.ts
@@ -1,43 +1,70 @@
-import { Injectable } from "@angular/core";
-import { MatSnackBar } from "@angular/material/snack-bar";
-import { HttpClient } from "@angular/common/http";
-import { Product } from "./product.model";
-import { Observable, EMPTY } from "rxjs";
+import { catchError, map } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { HttpClient } from '@angular/common/http';
+import { Product } from './product.model';
+import { EMPTY, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  baseUrl = "http://localhost:3001/productus";
+  baseUrl = "http://localhost:3001/products"
 
-  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
+  constructor(private snackBar: MatSnackBar,
+    private http: HttpClient) { }
 
-  showMessage(msg: string): void{
+  showMessage(msg: string, isError: boolean = false): void {
     this.snackBar.open(msg, 'X', {
       duration: 3000,
-      horizontalPosition: 'right',
-      verticalPosition: 'top'
+      horizontalPosition: "right",
+      verticalPosition: "top",
+      panelClass: isError ? ['msg-error'] : ['msg-success']
     })
   }
 
   create(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.baseUrl, product)
+    return this.http.post<Product>(this.baseUrl, product).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
   }
-  read(): Observable<Product[]>{
-    return this.http.get<Product[]>(this.baseUrl)
+  
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
   }
-  readById(id: string): Observable<Product>{
-    const url = `${this.baseUrl}/${id}` // está sendo concatenado a url da lista deprodutos, com / e o identificar do produto
-    return this.http.get<Product>(url)
+
+  readById(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.get<Product>(url).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
   }
-  update(product: Product): Observable<Product>{
+
+  update(product: Product): Observable<Product> {
     const url = `${this.baseUrl}/${product.id}`
-    return this.http.put<Product>(url, product)
+    return this.http.put<Product>(url, product).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
   }
+
   delete(id: number): Observable<Product>{
     const url = `${this.baseUrl}/${id}`
-    return this.http.delete<Product>(url);
+    return this.http.delete<Product>(url).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
+  }
+
+  errorHandler(e: any): Observable<any> {
+    this.showMessage('Ocorreu um erro!', true)
+    return EMPTY
   }
 }
 //VOID: ele identifica que a function não retorna nada, normalmente são function que precisam ser executadas mas não de retorno
