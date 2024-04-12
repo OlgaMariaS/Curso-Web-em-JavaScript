097462c8c8742fca2db3942d26180b32949c0aff
Olga Maria <olgamariasssantos@gmail.com>
2024-04-12 16:21:19 -0300

    Conclusão da seção 3

---
+++ secao 3 - Iniciando com a programacao/Trabalhando_com_dados_1_e_2.js
@@ -0,0 +1,7 @@
+var pedacos = 12; 
+//variaveis armazenam e são variaveis pois são modificaveis
+//existe outros tipo, exemplo let
+
+console.log("A pizza tem " + pedacos + " pedaços");
+
+//definir bons nomes ao arquivos e as senteças
